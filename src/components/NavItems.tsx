07f611611b079b02b3c57c9ba4e743d870251828
Ismum Chowdhury <ismumchowdhury@macbook-pro-5.mynetworksettings.com>
2023-12-13 20:30:51 -0500

    initial commit

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from "react";
+
+
+const NavItems = () => {
+const [active, setActive] = useState<null | number> (null);
+    return (
+        <div className="flex gap-4 h-full">
+            HI
+        </div>
+    );
+    }
+
+export default NavItems;
